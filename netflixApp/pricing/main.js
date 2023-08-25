3e0c9c31409324dbc4970c8fbdaf29a061a1a5dd
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-08-25 07:45:18 -0500

    agregando contenido y tablas al js

--- netflixApp/pricing/main.js
+++ netflixApp/pricing/main.js
@@ -1,6 +1,6 @@
 const url = "config"
 
-const ConstruirHead = async () => {
+const construirHead = async () => {
     let peticion = await fetch(`${url}.json`)
     let peticionJson = await peticion.json()
     let domElement = document.querySelector('#contTitle')
@@ -13,16 +13,15 @@ const ConstruirHead = async () => {
     `)
 }
 
-
-const ConstruirCards = async () => {
+const construirCards = async () => {
     let peticion = await fetch(`${url}.json`)
     let peticionJson = await peticion.json()
     let domElement = document.querySelector('#cardsCont')
     domElement.insertAdjacentHTML('beforeend', /* html */`
         ${peticionJson.main.sectionCard.cards.map((value) => /* html */ `
-            <div class="col">
-                <div class= "${value.cardBor}">
-                    <div class="${value.cardHeadBor}">
+            <div class="col mb-5">
+                <div class= "card mb-5 shadow-sm ${value.cardBor}">
+                    <div class="card-header py-3 ${value.cardHeadBor}">
                         <h4 class="my-0 fw-normal">${value.tituloCard}</h4>
                     </div>
                 </div>
@@ -34,7 +33,7 @@ const ConstruirCards = async () => {
                         <li>${value.infoCard3}</li>
                         <li>${value.infoCard4}</li>
                     </ul>
-                    <button type="button" class="${value.btnStyle}">Get started </button>
+                    <button type="button" class="w-100 btn btn-lg ${value.btnStyle}">Get started </button>
                 </div>
             </div>
         `).join(" ")
@@ -43,7 +42,56 @@ const ConstruirCards = async () => {
     console.log(peticionJson.main.sectionCard);
 }
 
+const construirArti = async () => {
+    let peticion = await fetch(`${url}.json`)
+    let peticionJson = await peticion.json()
+    let domElement = document.querySelector('#artiCont')
+    domElement.insertAdjacentHTML('beforeend', /* html */ `
+        <h2 class="display-6 text-center my-5">
+            Compare plans
+            ${peticionJson.main.sectionArti.title}
+        </h2>
+        <p class="my-5 text-center">
+            ${peticionJson.main.sectionArti.parrContent}
+        </p>
+    `)
+}
+
+const construirTable = async () => {
+    let peticion = await fetch(`${url}.json`)
+    let peticionJson = await peticion.json()
+    let domElement = document.querySelector('#mainTable')
+    domElement.insertAdjacentHTML('beforeend', /* html */`
+        <table class="table text-center align-middle">
+            <thead class="table text-center align-middle">
+                <tr class="align-middle">
+                ${peticionJson.main.sectionTable.tableHead.map((head) => /* html */`
+                <th style="width: ${head.width} ;">${head.title}</th>                
+                `).join(" ")}
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    ${peticionJson.main.sectionTable.tableBody.map((body) => /* html */`
+                    <th scope="row" class="text-center">Basic</th>
+                    <td><svg class="bi" width="24" height="24">
+                    </svg></td>
+                    <td><svg class="bi" width="24" height="24">
+                        <use xlink:href="#check" />
+                    </svg></td>
+                    <td><svg class="bi" width="24" height="24">
+                    </svg></td>
+                    <td><svg class="bi" width="30" height="24">
+                    </svg></td>
+                    `)}
+                </tr>
+            </tbody>
+        </table>
+    `)
+}
 
  
-ConstruirHead()
-ConstruirCards()
+construirHead()
+construirCards()
+construirArti()
+construirTable()
